1e16741f981f9e28024127ad87702abf54714f4c
lynekpng <lynekpng@gmail.com>
2024-09-17 11:27:27 +0200

    added future icons + layout fix

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,13 @@
 import { fetchWeatherData } from './weatherModule.js';
 import { updateWeatherUI } from './uiModule.js';
+import { darkMode } from './darkMode.js';
 
+
+document.addEventListener("DOMContentLoaded", () => {
+    darkMode(); 
+ 
+     
+ });
 document.getElementById('search-button').addEventListener('click', () => {
     const locationInput = document.getElementById('location-input').value;
     if (locationInput) {
@@ -14,9 +21,23 @@ document.getElementById('location-input').addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
         const locationInput = e.target.value;
         if (locationInput) {
+           
             fetchWeatherData(locationInput).then(weatherData => {
-                updateWeatherUI(weatherData);
+                if (weatherData) {
+                    console.log('Weather data fetched:', weatherData); // Log the full object to the console
+                    updateWeatherUI(weatherData); // Update the UI with the fetched data
+                    localStorage.setItem('location', JSON.stringify(locationInput)); // Save location to localStorage
+                } else {
+                    console.error('No weather data to display'); // Handle case where data is null
+                }
+            }).catch(error => {
+                console.error('Error fetching weather data:', error); // Log any errors
             });
-        }
-    }
-});
+            
+
+        }}});
+
+     
+
+
+        
